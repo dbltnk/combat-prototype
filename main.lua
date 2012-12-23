0193fc89093d8fff1c5a0ffa352bacfa0b5a48c5
dbltnk <alexander.zacherl@googlemail.com>
2012-12-23 14:47:06 +0100

    added character and mouse with controls

--- main.lua
+++ main.lua
@@ -2,3 +2,51 @@ STRICT = true
 DEBUG = true
 
 require 'zoetrope'
+
+Player= Tile:extend
+{
+	width = 32,
+	height = 32,
+	image = '/assets/graphics/player.png',
+--	rotation = 1,
+	
+	onUpdate = function (self, elapsed)
+                if the.keys:pressed('left', 'a') then
+			self.velocity.x = -200
+                elseif the.keys:pressed('right', 'd') then
+			self.velocity.x = 200
+                elseif the.keys:pressed('up', 'w') then
+			self.velocity.y = -200
+                elseif the.keys:pressed('down', 's') then
+			self.velocity.y = 200			
+                else
+			self.velocity.x = 0
+			self.velocity.y = 0
+		end
+		rotation = math.atan2((self.y-the.mouse.y),(self.x-the.mouse.x))
+--		print(rotation)
+	end
+}
+
+Cursor= Tile:extend
+{
+	width = 32,
+	height = 32,
+	image = '/assets/graphics/cursor.png',
+    
+        onUpdate = function (self)
+		self.x = the.mouse.x - 16
+		self.y = the.mouse.y - 16
+	end
+}
+
+the.app = App:new
+{
+    onRun = function (self)
+        self.player = Player:new{ x = 300, y = 300 }
+        self:add(self.player)
+	self.cursor = Cursor:new{ x = 0, y = 0 }
+        self:add(self.cursor)
+	self:useSysCursor(false)
+    end
+}
