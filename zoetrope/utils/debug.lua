663c41ea9bb0ac54b0722c06005c23dc52b46124
Sebastian Dorda <hagish@schattenkind.net>
2013-04-24 01:30:45 +0200

    strip console log to save size

--- zoetrope/utils/debug.lua
+++ zoetrope/utils/debug.lua
@@ -121,12 +121,13 @@ DebugConsole = Group:extend{
 				obj.log.text = obj.log.text .. tostring(value) .. ' '
 			end
 
+			obj.log.text = obj.log.text .. '\n'
+
 			-- trim to sane size
-			if obj.log.text:len() > 20*100 then 
+			if obj.log.text and obj.log.text:len() > 20*100 then 
 				obj.log.text = obj.log.text(obj.log.text:len() - 20*100) 
 			end
 
-			obj.log.text = obj.log.text .. '\n'
 			obj._updateLog = true
 			obj._oldPrint(...)
 		end
