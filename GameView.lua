76610ae74b32935b212e32261ebb495c6ce7c031
Sebastian Dorda <hagish@schattenkind.net>
2013-05-24 11:14:21 +0200

    fixes #173 wrong teams

--- GameView.lua
+++ GameView.lua
@@ -121,7 +121,11 @@ GameView = View:extend
 		
 		-- setup player
 		the.player = Player:new{ x = the.app.width / 2, y = the.app.height / 2, 
-			name = localconfig.playerName, armor = localconfig.armor, weapon = localconfig.weapon }
+			name = localconfig.playerName, 
+			armor = localconfig.armor, 
+			weapon = localconfig.weapon,
+			team = localconfig.team,
+		 }
 		
     -- place ontop
 		self:remove(self.trees)
