b78bd4a429876d5348ab56141725f40885f1cc11
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-05-24 16:52:49 +0200

    bat hardcorded

--- buildwin32.bat
+++ buildwin32.bat
@@ -2,12 +2,15 @@
 md buildtmp
 copy localconfig.lua.dist "%~dp0\buildtmp\localconfig.lua"
 copy readme.md "%~dp0\buildtmp\readme.txt"
-winrar a -r -x*\server -x*\build_material -x*\buildtmp -x@"%~dp0\build_material\buildexclude.txt" "%~dp0\buildtmp\game.zip"
+"C:\Program Files\WinRAR\WinRAR.exe" a -r -x*\server -x*\build_material -x*\buildtmp -x@"%~dp0\build_material\buildexclude.txt" "%~dp0\buildtmp\game.zip"
 copy "%~dp0\buildtmp\game.zip" "%~dp0\buildtmp\game.love"
 del "%~dp0\buildtmp\game.zip"
 copy "%~dp0\build_material\lovewin32\*.*" "%~dp0\buildtmp\"
 copy /b "%~dp0\buildtmp\love.exe"+"%~dp0\buildtmp\game.love" "%~dp0\buildtmp\dastal_proto1.exe"
 del "%~dp0\buildtmp\game.love"
 del "%~dp0\buildtmp\love.exe"
-winrar a -ep1 dastal_proto1_win32.zip buildtmp\*
+"C:\Program Files\WinRAR\WinRAR.exe" a -ep1 dastal_proto1_win32.zip buildtmp\*
 rd /s /q buildtmp
+pause
+
+
