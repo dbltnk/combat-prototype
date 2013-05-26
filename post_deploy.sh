b68a744ecfc5e34fb36d14011e893b1e8c96f4fc
Sebastian Dorda <hagish@schattenkind.net>
2013-05-27 00:09:11 +0200

    changelog

--- post_deploy.sh
+++ post_deploy.sh
@@ -5,6 +5,12 @@ cd server
 npm install
 cd ..
 
+echo changelog
+echo -n `git log --pretty=oneline|wc -l` > changelog.txt
+echo " commits" >> changelog.txt
+echo "------------" >> changelog.txt
+git log --pretty=oneline --abbrev-commit --since "2 weeks" >> changelog.txt
+
 echo build client win
 rm -rf buildtmp
 mkdir buildtmp
@@ -16,6 +22,7 @@ cp build_material/lovewin32/* buildtmp
 cat buildtmp/love.exe buildtmp/game.love > buildtmp/dastal_proto1.exe
 rm buildtmp/game.love
 rm buildtmp/love.exe
+cp changelog.txt buildtmp
 mv buildtmp dastal_proto1
 zip -r dastal_proto1.zip dastal_proto1
 mkdir -p build
@@ -23,3 +30,4 @@ rm -rf build/dastal_proto1
 mv dastal_proto1 build
 mv dastal_proto1.zip build
 rm -rf buildtmp
+
