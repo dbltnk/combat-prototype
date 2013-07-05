fc62143de841b7d0a7dd49aa5f293461bca0c5f5
Sebastian Dorda <hagish@schattenkind.net>
2013-07-05 13:44:18 +0200

    more graphs :)

--- log/plot.sh
+++ log/plot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function plot {
-echo "set title '$1'"                     >  "$1.gnu"
+echo "set title '$2 - $1'"                     >  "$1.gnu"
 echo "set key box"                        >>  "$1.gnu"
 echo "set key bottom right"                    >>  "$1.gnu"
 echo "set size 1,1"                        >>  "$1.gnu"
@@ -9,15 +9,15 @@ echo "set xlabel 'time'"                    >>  "$1.gnu"
 echo "set ylabel '$1'"            >>  "$1.gnu"
 echo "set autoscale"                        >>  "$1.gnu"
 echo "set term png"                    >>  "$1.gnu"
-echo "set output '$1.png'"    >>  "$1.gnu"
+echo "set output '$2_$1.png'"    >>  "$1.gnu"
 echo -n "plot " >> "$1.gnu"
 
 mysql --column-names=0 -u root dastal << EOF | while read NAME
-select distinct name from player_ot
+select distinct $3 from $2
 EOF
 do
 	mysql --column-names=0 -u root dastal << EOF > $1_$NAME.dat
-select time - (SELECT MIN(time) FROM t) as time, $1 from player_ot where name = '$NAME'
+select time - (SELECT MIN(time) FROM t) as time, $1 from $2 where $3 = '$NAME'
 EOF
 echo -n "\"$1_$NAME.dat\" using 1:2 title \"$NAME\", " >>  "$1.gnu"
 done
@@ -28,12 +28,20 @@ echo >> "$1.gnu"
 gnuplot < "$1.gnu"
 }
 
-plot "xp"
-plot "level"
-plot "death"
-plot "kills_player"
-plot "xp_combat"
-plot "xp_creeps"
-plot "xp_resources"
-plot "barrier_dmg"
-plot "resources_dmg"
+rm -f *png
+
+plot "xp" "player_ot" "name"
+plot "xp_sum" "player_ot" "name"
+plot "level" "player_ot" "name"
+plot "death" "player_ot" "name"
+plot "kills_player" "player_ot" "name"
+plot "xp_combat" "player_ot" "name"
+plot "xp_creeps" "player_ot" "name"
+plot "xp_resources" "player_ot" "name"
+plot "barrier_dmg" "player_ot" "name"
+plot "resources_dmg" "player_ot" "name"
+plot "currentPain" "barrier_ot" "event"
+#~ plot "controller" "resource_ot" '`desc`'
+
+rm -f *gnu *dat
+
--- log/queries.sql
+++ log/queries.sql
@@ -3,23 +3,61 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2013 at 02:09 AM
+-- Generation Time: Jul 05, 2013 at 01:43 PM
 -- Server version: 5.5.31-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `dastal`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `barrier_ot`
+--
+DROP VIEW IF EXISTS `barrier_ot`;
+CREATE TABLE IF NOT EXISTS `barrier_ot` (
+`session` int(10) unsigned
+,`time` int(10) unsigned
+,`event` varchar(255)
+,`clientid` varchar(255)
+,`currentPain` varchar(255)
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `config`
+--
+DROP VIEW IF EXISTS `config`;
+CREATE TABLE IF NOT EXISTS `config` (
+`session` int(10) unsigned
+,`time` int(10) unsigned
+,`event` varchar(255)
+,`clientid` varchar(255)
+,`name` varchar(255)
+,`team` varchar(255)
+,`fullscreen` varchar(255)
+,`screenWidth` varchar(255)
+,`screenHeight` varchar(255)
+,`max_chat_lines` varchar(255)
+,`audioVolume` varchar(255)
+,`skillOne` varchar(255)
+,`skillTwo` varchar(255)
+,`skillThree` varchar(255)
+,`skillFour` varchar(255)
+,`skillFive` varchar(255)
+,`skillSix` varchar(255)
+,`skillSeven` varchar(255)
+,`skillEight` varchar(255)
+,`targetSelf` varchar(255)
+,`showHighscore` varchar(255)
+,`toggleFullscreen` varchar(255)
+,`quitGame` varchar(255)
+);
 -- --------------------------------------------------------
 
 --
@@ -42,6 +80,7 @@ CREATE TABLE IF NOT EXISTS `player_ot` (
 ,`xp_resources` bigint(21)
 ,`barrier_dmg` bigint(21)
 ,`resources_dmg` bigint(21)
+,`xp_sum` bigint(21)
 );
 -- --------------------------------------------------------
 
@@ -87,6 +126,49 @@ CREATE TABLE IF NOT EXISTS `raw` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `resource_ot`
+--
+DROP VIEW IF EXISTS `resource_ot`;
+CREATE TABLE IF NOT EXISTS `resource_ot` (
+`session` int(10) unsigned
+,`time` int(10) unsigned
+,`event` varchar(255)
+,`clientid` varchar(255)
+,`oid` varchar(255)
+,`desc` varchar(255)
+,`currentPain` varchar(255)
+,`controller` varchar(255)
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `skill_taken`
+--
+DROP VIEW IF EXISTS `skill_taken`;
+CREATE TABLE IF NOT EXISTS `skill_taken` (
+`session` int(10) unsigned
+,`time` int(10) unsigned
+,`event` varchar(255)
+,`clientid` varchar(255)
+,`name` varchar(255)
+,`skill` varchar(255)
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `skill_used`
+--
+DROP VIEW IF EXISTS `skill_used`;
+CREATE TABLE IF NOT EXISTS `skill_used` (
+`session` int(10) unsigned
+,`time` int(10) unsigned
+,`event` varchar(255)
+,`clientid` varchar(255)
+,`skill` varchar(255)
+);
+-- --------------------------------------------------------
+
+--
 -- Stand-in structure for view `t`
 --
 DROP VIEW IF EXISTS `t`;
@@ -96,11 +178,56 @@ CREATE TABLE IF NOT EXISTS `t` (
 -- --------------------------------------------------------
 
 --
+-- Structure for view `barrier_ot`
+--
+DROP TABLE IF EXISTS `barrier_ot`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `barrier_ot` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `currentPain` from `raw` where (`raw`.`event` = 'barrier_ot');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `config`
+--
+DROP TABLE IF EXISTS `config`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `config` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `name`,`raw`.`p3` AS `team`,`raw`.`p4` AS `fullscreen`,`raw`.`p5` AS `screenWidth`,`raw`.`p6` AS `screenHeight`,`raw`.`p7` AS `max_chat_lines`,`raw`.`p8` AS `audioVolume`,`raw`.`p9` AS `skillOne`,`raw`.`p10` AS `skillTwo`,`raw`.`p11` AS `skillThree`,`raw`.`p12` AS `skillFour`,`raw`.`p13` AS `skillFive`,`raw`.`p14` AS `skillSix`,`raw`.`p15` AS `skillSeven`,`raw`.`p16` AS `skillEight`,`raw`.`p17` AS `targetSelf`,`raw`.`p18` AS `showHighscore`,`raw`.`p19` AS `toggleFullscreen`,`raw`.`p19` AS `quitGame` from `raw` where (`raw`.`event` = 'config');
+
+-- --------------------------------------------------------
+
+--
 -- Structure for view `player_ot`
 --
 DROP TABLE IF EXISTS `player_ot`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `player_ot` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `oid`,`raw`.`p3` AS `name`,cast(`raw`.`p4` as signed) AS `level`,cast(`raw`.`p5` as signed) AS `xp`,cast(`raw`.`p6` as signed) AS `death`,cast(`raw`.`p7` as signed) AS `kills_player`,cast(`raw`.`p8` as signed) AS `xp_combat`,cast(`raw`.`p9` as signed) AS `xp_creeps`,cast(`raw`.`p10` as signed) AS `xp_resources`,cast(`raw`.`p11` as signed) AS `barrier_dmg`,cast(`raw`.`p12` as signed) AS `resources_dmg` from `raw` where (`raw`.`event` = 'player_ot');
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `player_ot` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `oid`,`raw`.`p3` AS `name`,cast(`raw`.`p4` as signed) AS `level`,cast(`raw`.`p5` as signed) AS `xp`,cast(`raw`.`p6` as signed) AS `death`,cast(`raw`.`p7` as signed) AS `kills_player`,cast(`raw`.`p8` as signed) AS `xp_combat`,cast(`raw`.`p9` as signed) AS `xp_creeps`,cast(`raw`.`p10` as signed) AS `xp_resources`,cast(`raw`.`p11` as signed) AS `barrier_dmg`,cast(`raw`.`p12` as signed) AS `resources_dmg`,cast(((`raw`.`p8` + `raw`.`p9`) + `raw`.`p10`) as signed) AS `xp_sum` from `raw` where (`raw`.`event` = 'player_ot');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `resource_ot`
+--
+DROP TABLE IF EXISTS `resource_ot`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `resource_ot` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `oid`,`raw`.`p3` AS `desc`,`raw`.`p4` AS `currentPain`,`raw`.`p5` AS `controller` from `raw` where (`raw`.`event` = 'resource_ot');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `skill_taken`
+--
+DROP TABLE IF EXISTS `skill_taken`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `skill_taken` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `name`,`raw`.`p3` AS `skill` from `raw` where (`raw`.`event` = 'skill_taken');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `skill_used`
+--
+DROP TABLE IF EXISTS `skill_used`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `skill_used` AS select `raw`.`session` AS `session`,`raw`.`time` AS `time`,`raw`.`event` AS `event`,`raw`.`p1` AS `clientid`,`raw`.`p2` AS `skill` from `raw` where (`raw`.`event` = 'skill_used');
 
 -- --------------------------------------------------------
 
@@ -110,7 +237,3 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 DROP TABLE IF EXISTS `t`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `t` AS select distinct `raw`.`time` AS `time` from `raw`;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
