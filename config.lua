c5a1ae0b96e47428dfd68a41f8ad9e781b37b500
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-09-27 11:40:22 +0200

    increased ressource xp by 50%

--- config.lua
+++ config.lua
@@ -33,7 +33,7 @@ config.xpPerRessourceTick = config.xpCap * config.levelCap / config.roundTime * 
 config.strIncreaseFactor = 0.1 -- 10% stronger per lvl
 config.combatHealXP = 0.15 -- in % of damage / heal done
 config.crowdControlXP = 0.2 -- in % of duration in seconds
-config.ressourceQualityTable = {0.5, 0.5, 1, 2}
+config.ressourceQualityTable = {0.5, 1, 1.5, 3}
 config.numberOfMaps = 4
 config.mapNumber = 0 -- 0 means random
 
