eb5bc99dea4227c6b28e34d9be98df6555d82923
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-11-09 15:49:10 +0100

    fixed camera bug

--- FocusSprite.lua
+++ FocusSprite.lua
@@ -12,9 +12,11 @@ FocusSprite = Sprite:extend
 		-- weighted average
 		x,y = vector.add(x,y, vector.mul(worldCursorX, worldCursorY, 0.45))
 		x,y = vector.add(x,y, vector.mul(the.player.x, the.player.y, 0.55))
+		
 		-- don't go beyond the map borders
 		self.x = utils.clamp(x,love.graphics.getWidth() / 2, 3200 - love.graphics.getWidth() / 2) -- TODO: make this dynamic, map size currently 3200x3200
 		self.y = utils.clamp(y,love.graphics.getHeight() / 2, 3200 - love.graphics.getHeight() / 2) -- TODO: make this dynamic, map size currently 3200x3200 
+		
 		-- equal sight range in both x and y directions
 		local px, py = tools.object_center (the.player)
 		local cap = math.min(love.graphics.getWidth(), love.graphics.getHeight()) / config.focusSpriteMaxRange
@@ -28,6 +30,14 @@ FocusSprite = Sprite:extend
 			self.x = utils.clamp(self.x, px - cap, px + cap)
 			self.y = utils.clamp(self.y, py - cap * widthToHeightRatio, py + cap * widthToHeightRatio)
 		end
+		
+		-- dirty hack to prevent the camera from leaving the map
+		w = love.graphics.getWidth()
+		h = love.graphics.getHeight()
+		if self.x < w/2 then self.x = w/2 end
+		if self.y < h/2 then self.y = h/2 end
+		if self.x > 3200 - w/2 then self.x = 3200 - w/2 end
+		if self.y > 3200 - h/2 then self.y = 3200 - h/2 end
 	end,
 	
 	__tostring = function (self)
